234af423337cbde6c02cb5174bd86758a9c7563d
Łukasz Orluk <s197641@student.pg.edu.pl>
2024-05-13 01:52:19 +0200

    chyba dft dziala w porzadku

--- Signal_processing.cpp
+++ Signal_processing.cpp
@@ -7,62 +7,62 @@
 
 #define M_PI 3.14159265358979323846
 
-float add(float i, float j) {
-    return i + j;
-}
-
-using namespace std;
-//spectrum - liczba zespolona ofc ;)) jutro do dokoncze raczej
-
-
-vector<double> dft(const vector<double>& x) 
+void sin_transform_plot(double amplitude, double frequency)
 {
-    int N = x.size();
-    vector<double> spectrum(N, 0.0);
-    for (int k = 0; k < N; ++k) {
-        complex<double> sum{ 0.0, 0.0 };
+    using namespace matplot;
+    using namespace std;
+    int N = 1000; 
+    double duration = 2 * M_PI;
+    double sampling_rate = N / duration;
+
+    vector<double> time;
+    for (int i = 0; i < N; ++i) {
+        time.push_back(i / sampling_rate);
+    }
+
+    vector<double> signal; // wartosci sinusa
+    for (int i = 0; i < time.size(); ++i) {
+        double t = time[i];
+        signal.push_back(amplitude * sin(2 * M_PI * frequency * t));
+    }
+
+    
+    vector<complex<double>> dft_result; // tu oblczam dft
+    for (int k = 0; k < N; ++k) 
+    {
+        complex<double> sum(0.0, 0.0);
+
         for (int n = 0; n < N; ++n) {
-            double angle = -2.0 * M_PI * k * n / N;
-            std::complex<double> exp_term(cos(angle),sin(angle));
-            sum += x[n] * exp_term;
+            sum += signal[n] * exp(-2.0 * M_PI * complex<double>(0, 1) * static_cast<double>(n * k) / static_cast<double>(N)); // rownanie wrzucone na wiki 
         }
-        spectrum[k] = abs(sum); 
+        dft_result.push_back(sum / static_cast<double>(N));
     }
 
-    return spectrum;
-}
+    vector<double> frequencies;
+    for (int k = 0; k < N; ++k) 
+    {
+        double freq;
+        if (k <= N / 2) {
+            freq = static_cast<double>(k) * sampling_rate / N;
+        }
+        else {
+            freq = -(sampling_rate - static_cast<double>(k)) * sampling_rate / N;
+        }
+        frequencies.push_back(freq);
+    }
 
-void plot_spectrum(const std::vector<double>& spectrum) {
-    using namespace matplot;
-    auto N = spectrum.size();
-    auto f = linspace(0, 0.5, N); // Wektor częstotliwości
-    plot(f, spectrum);
-    xlabel("Frequency");
+    vector<double> real_parts;
+    vector<double> imaginary_parts;
+    for (int k = 0; k < N; ++k) {
+        real_parts.push_back(abs(dft_result[k].real()) * 2);
+        imaginary_parts.push_back(abs(dft_result[k].imag()) * 2);
+    }
+
+    plot(frequencies, real_parts);
+    plot(frequencies, imaginary_parts);
+    title("DFT Results");
+    xlabel("Frequency (Hz)");
     ylabel("Amplitude");
-    title("Amplitude Spectrum");
-    show();
-}
-
-void sin_transform_plot(double amplitude, double freq, double time)
-{
-    using namespace matplot;
-    auto t = linspace(0, time, 1000); 
-    vector<double> y_values; 
-    for (auto x : t) {
-        y_values.push_back(amplitude * sin(2 * M_PI * freq * x));
-    }
-    auto spectrum = dft(y_values);
-    plot_spectrum(spectrum);
-}
-
-void test_plot()
-{
-    using namespace matplot;
-    auto [X, Y] = meshgrid(iota(-3, .125, 3));
-    auto Z = peaks(X, Y);
-    auto C = transform(X, Y, [](double x, double y) { return x * y; });
-    surfc(X, Y, Z, C);
-    colorbar();
 
     show();
 }
@@ -72,7 +72,5 @@ namespace py = pybind11;
 PYBIND11_MODULE(Signal, m)
 {
     m.doc() = "Signal processing";
-    m.def("add", &add, "A function which adds two numbers");
-    m.def("test_plot", &test_plot, "test");
-    m.def("sin_transform_plot", &sin_transform_plot, "zmiana sygnalu dft");
+    m.def("sin_transform_plot", &sin_transform_plot, "Transforms and plots sinusoidal signal using DFT");
 }
